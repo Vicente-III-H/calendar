0e746dc23cd4e381e1b806eef8b53ab6ddec6df3
Vicente <145728917+Vicente-III-H@users.noreply.github.com>
2025-07-30 18:33:56 -0400

    Add basic calendar structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Calendar from './calendar'
+
+function App() {
+  return (
+    <div className='container'>
+      <Calendar></Calendar>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/calendar.jsx
@@ -0,0 +1,61 @@
+import './calendar.css'
+
+function Day() {
+    return (
+        <div className='day'>
+            <div className="day-number">#</div>
+            <div className="space"></div>
+        </div>
+    )
+}
+
+function Calendar() {
+    return (
+        <div className="calendar">
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+            <Day></Day>
+        </div>
+    )
+}
+
+export default Calendar
