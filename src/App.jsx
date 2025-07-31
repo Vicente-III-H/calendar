021a31d053cdaa4e5f1b1e08c4eda8fb52010e85
Vicente <145728917+Vicente-III-H@users.noreply.github.com>
2025-07-30 20:23:34 -0400

    Add base calendar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import './App.css'
 import Calendar from './calendar'
 
 function App() {
+  const [calendarMonth, setCalendarMonth] = useState(0);
+  
+  const changeMonth = (increase = true) => {
+    const newMonth = increase ? calendarMonth + 1 : calendarMonth - 1;
+    setCalendarMonth(newMonth);
+  }
+
   return (
     <div className='container'>
-      <Calendar></Calendar>
-      
+      <Calendar month={calendarMonth}></Calendar>
+      <button onClick={() => {changeMonth(false)}}>prev. month</button>
+      <button onClick={() => {setCalendarMonth(0)}}>today</button>
+      <button onClick={changeMonth}>next month</button>
     </div>
   )
 }
--- src/calendar.jsx
+++ src/calendar.jsx
@@ -1,59 +1,33 @@
 import './calendar.css'
 
-function Day() {
+function Day(props) {
     return (
         <div className='day'>
-            <div className="day-number">#</div>
+            <div className="day-number">{props.day}</div>
             <div className="space"></div>
         </div>
     )
 }
 
-function Calendar() {
+function Calendar(props) {
+    const calendarDays = (() => {
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = today.getMonth() + props.month;
+
+        const startingDay = (new Date(year, month, 1)).getDay();
+        
+        let dates = []
+        for (let counter = 0; counter < 42; counter++) {
+            dates.push(new Date(year, month, 1 - startingDay + counter))
+        }
+
+        return dates;
+    })();
+
     return (
         <div className="calendar">
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
-            <Day></Day>
+            {calendarDays.map((day) => (<Day key={day} day={day.getDate()}></Day>))}
         </div>
     )
 }
