8583c45ab2c22b8e94e52ab1694299bcd68d4c74
Vicente <145728917+Vicente-III-H@users.noreply.github.com>
2025-08-09 12:22:19 -0400

    Add event card code

--- src/events/events.jsx
+++ src/events/events.jsx
@@ -2,17 +2,26 @@ import { useState } from "react";
 import Modal from "./modal";
 import "./events.css"
 
+function EventCard({ event }) {
+    /* finish */
+    return (
+        <div>
+            {event.name}
+        </div>
+    )
+}
+
 function Events() {
     const [showModal, setShowModal] = useState(false);
     const [events, setEvents] = useState([]);
     const addToEvents = (value) => {
-        let newArray = [...events];
-        newArray.push(value);
+        let newArray = [...events, value];
         newArray.sort((a, b) => { return (new Date(a.date)) - (new Date(b.date)) });
         setEvents(newArray);
     };
 
     const saveEvent = (event) => {
+        /* add validation */
         event.id = crypto.randomUUID();
         addToEvents(event);
     }
@@ -25,9 +34,7 @@ function Events() {
                     <button onClick={() => {setShowModal(true)}}>+</button>
                 </div>
                 <div className="flex-grow">
-                    {events.map((event) => {
-                        return (<div key={event.id}>{event.name}</div>)
-                    })}
+                    {events.map((event) => <EventCard key={event.id} event={event}></EventCard>)}
                 </div>
             </div>
             <Modal showModal={showModal} setShowModal={setShowModal} saveEvent={saveEvent}></Modal>
