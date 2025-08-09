6442d1dbb43296803b5949c188af02935c79b754
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2025-08-09 14:01:28 -0400

    Add event cards

--- src/events/events.jsx
+++ src/events/events.jsx
@@ -3,10 +3,32 @@ import Modal from "./modal";
 import "./events.css"
 
 function EventCard({ event }) {
-    /* finish */
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+    };
+    const formatTime = (dateString) => {
+        const date = new Date(dateString);
+        return date.getHours() + ":" + date.getMinutes();
+    };
+
     return (
-        <div>
-            {event.name}
+        <div className="event-card flexbox-column">
+            <div className="event-card-header">
+                <div style={{backgroundColor: event.color}} className="colour-box"></div>
+                <div>{event.name}</div>
+            </div>
+            <div>
+                {formatDate(event.date)}
+            </div>
+            <div>
+                {formatTime(event.date)}
+            </div>
+            <div>
+                {event.notes}
+            </div>
         </div>
     )
 }
