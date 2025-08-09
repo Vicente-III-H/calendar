82a366ed1121f884066110cac58f67027d593157
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2025-08-09 19:38:12 -0400

    Make events removable

--- src/events/events.jsx
+++ src/events/events.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Modal from "./modal";
 import "./events.css"
 
-function EventCard({ event }) {
+function EventCard({ event, deleteEvent }) {
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     const formatDate = (dateString) => {
@@ -18,7 +18,8 @@ function EventCard({ event }) {
         <div className="event-card flexbox-column">
             <div className="event-card-header">
                 <div style={{backgroundColor: event.color}} className="colour-box"></div>
-                <div>{event.name}</div>
+                <div className="flex-grow">{event.name}</div>
+                <button onClick={() => {deleteEvent(event)}}>-</button>
             </div>
             <div>
                 {formatDate(event.date)}
@@ -33,7 +34,7 @@ function EventCard({ event }) {
     )
 }
 
-function EventSection({ title, eventList }) {
+function EventSection({ title, eventList, deleteEvent }) {
     if (eventList.length === 0) {
         return null;
     }
@@ -42,7 +43,7 @@ function EventSection({ title, eventList }) {
         <div>
             <div>{title}</div>
             <div className="flex-grow">
-                {eventList.map((event) => <EventCard key={event.id} event={event}></EventCard>)}
+                {eventList.map((event) => <EventCard key={event.id} event={event} deleteEvent={deleteEvent}></EventCard>)}
             </div>
         </div>
     )
@@ -68,6 +69,18 @@ function Events({ eventLists, setEventLists }) {
         newEventLists[selectList].sort((a, b) => { return (new Date(a.date)) - (new Date(b.date)) });
         setEventLists(newEventLists);
     };
+    const deleteEvent = (event) => {
+        const date = new Date(event.date);
+        let newEventLists = {...eventLists};
+        let selectList = "past";
+
+        if (isSameDay(date, eventLists.date)) { selectList = "today" }
+        else if (date > eventLists.date) { selectList = "upcoming" };
+
+        const deleteIndex = newEventLists[selectList].indexOf(event);
+        newEventLists[selectList].splice(deleteIndex, 1);
+        setEventLists(newEventLists);
+    };
     
     return (
         <>
@@ -77,9 +90,9 @@ function Events({ eventLists, setEventLists }) {
                     <button onClick={() => {setShowModal(true)}}>+</button>
                 </div>
                 <div id="events-section-container">
-                    <EventSection title="Past" eventList={eventLists.past}></EventSection>
-                    <EventSection title="Today" eventList={eventLists.today}></EventSection>
-                    <EventSection title="Upcoming" eventList={eventLists.upcoming}></EventSection>
+                    <EventSection title="Past" eventList={eventLists.past} deleteEvent={deleteEvent}></EventSection>
+                    <EventSection title="Today" eventList={eventLists.today} deleteEvent={deleteEvent}></EventSection>
+                    <EventSection title="Upcoming" eventList={eventLists.upcoming} deleteEvent={deleteEvent}></EventSection>
                 </div>
             </div>
             <Modal showModal={showModal} setShowModal={setShowModal} addToEvents={addToEvents}></Modal>
